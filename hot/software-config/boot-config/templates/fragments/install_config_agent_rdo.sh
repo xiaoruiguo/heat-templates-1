60a49f2ad2621a84d174df6c78a8f583bd2584d9
root <root@kolla22.labs.local>
2017-05-23 14:15:25 +0800

    https://developer.rackspace.com/docs/user-guides/orchestration/generic-software-config/#generic-sw-config-usage

--- hot/software-config/boot-config/templates/fragments/install_config_agent_rdo.sh
+++ hot/software-config/boot-config/templates/fragments/install_config_agent_rdo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eux
-
+sed -i '$a proxy=http://opscloud:cloud0518@10.53.13.245:3128/' /etc/yum.conf
 yum -y install https://www.rdoproject.org/repos/rdo-release.rpm
 yum -y install python-zaqarclient os-collect-config os-apply-config os-refresh-config dib-utils
